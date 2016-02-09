2b124db265db2269ba87e9186a1793b5cbbd5fd3
ruijose <ruijosemangas@gmail.com>
2016-02-09 11:00:03 +0000

    added hyperty creation time tests

--- server/integration_tests.rb
+++ server/integration_tests.rb
@@ -14,6 +14,10 @@ describe 'domain registry api tests' do
     @hyperty_two_details = {
       descriptor: "asdasd112AA"
     }
+
+    @hyperty_three_details = {
+      descriptor: "12312321istuapt"
+    }
   }
 
   describe 'create user hyperty' do
@@ -41,14 +45,41 @@ describe 'domain registry api tests' do
       expect_json_keys("hyperty2", [:descriptor, :startingTime, :lastModified])
       expect_json_types("hyperty1", descriptor: :string, startingTime: :string, lastModified: :string)
       expect_json_types("hyperty2", descriptor: :string, startingTime: :string, lastModified: :string)
-      expect_json("hyperty1.descriptor", "kk11jasdasdAA")
-      expect_json("hyperty2.descriptor", "asdasd112AA")
+      expect(json_body[:hyperty1][:descriptor]).to eql("kkk11jasdasdAA")
+      expect(json_body[:hyperty2][:descriptor]).to eql("asdasd112AA")
+      expect(json_body[:hyperty1][:startingTime]).to eql(json_body[:hyperty1][:lastModified])
+      expect(json_body[:hyperty2][:startingTime]).to eql(json_body[:hyperty2][:lastModified])
     end
 
     it 'should return an error, user not found' do
       get '/nuno@inesc.pt'
       expect_status(400)
       expect_json(:message => "user not found")
+    end
+  end
+
+  describe 'update and get all hyperties' do
+
+    it 'should update a hyperty' do
+      sleep(1)
+      put '/ruijose@inesc.pt/hyperty1', @hyperty_three_details
+      expect_status(200)
+      expect_json(:message => "hyperty created")
+    end
+
+    it 'should get all updated hyperties' do
+      get '/ruijose@inesc.pt'
+      p json_body
+      expect_status(200)
+      expect_json_sizes(2)
+      expect_json_keys("hyperty1", [:descriptor, :startingTime, :lastModified])
+      expect_json_keys("hyperty2", [:descriptor, :startingTime, :lastModified])
+      expect_json_types("hyperty1", descriptor: :string, startingTime: :string, lastModified: :string)
+      expect_json_types("hyperty2", descriptor: :string, startingTime: :string, lastModified: :string)
+      expect(json_body[:hyperty1][:descriptor]).to eql("12312321istuapt")
+      expect(json_body[:hyperty2][:descriptor]).to eql("asdasd112AA")
+      expect(json_body[:hyperty2][:startingTime]).to eql(json_body[:hyperty2][:lastModified])
+      expect(json_body[:hyperty1][:startingTime]).to be < (json_body[:hyperty1][:lastModified])
     end
   end
 
