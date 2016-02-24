697ec1ad2814e1ad5e1c8d05d7324cabb4e1117d
ruijose <ruijosemangas@gmail.com>
2016-02-24 11:20:01 +0000

    updated unit tests examples

--- server/src/test/java/domainregistry/HypertyTest.java
+++ server/src/test/java/domainregistry/HypertyTest.java
@@ -27,76 +27,80 @@ import java.util.ArrayList;
 public class HypertyTest {
 
     private HypertyService services = null;
-    private HypertyInstance ins = null;
-    private String user;
+    private HypertyInstance hypertyInstance = null;
+    private String userID;
     private String hypertyID;
+    private String descriptor;
 
     @Before
     public void setData(){
-        user = "john@skype.com";
-        hypertyID = "12312jjjj12x";
         services = new HypertyService();
-        ins = new HypertyInstance();
-        ins.setDescriptor("asdasdasd");
-        services.createUserHyperty(user, hypertyID, ins);
+        hypertyInstance = new HypertyInstance();
+        userID = "user://inesc-id.pt/ruimangas";
+        hypertyID = "hyperty://ines-id.pt/123-Ha-123123";
+        descriptor = "hyperty-catalogue://inesc-id.pt/1/123-Ha-123123";
+        hypertyInstance.setDescriptor(descriptor);
+        services.createUserHyperty(userID, hypertyID, hypertyInstance);
     }
 
     @Test
     public void createUserTest(){
-        assertTrue(services.getServices().containsKey(user));
+        assertTrue(services.getServices().containsKey(userID));
     }
 
     @Test
     public void HypertyCreationTest(){
-        Map<String, HypertyInstance> servs = services.getServices().get(user);
-        assertTrue(servs.containsKey(hypertyID));
-        HypertyInstance i = servs.get(hypertyID);
-        assertEquals(ins, i);
+        Map<String, HypertyInstance> userHyperties = services.getServices().get(userID);
+        assertTrue(userHyperties.containsKey(hypertyID));
+        HypertyInstance userHypertyInstance = userHyperties.get(hypertyID);
+        assertEquals(hypertyInstance, userHypertyInstance);
     }
 
     @Test
     public void getAllExistingUserHypertiesTest(){
-      String newHypertyID = "123dasxxxxlkiII";
-      HypertyInstance hyperty = new HypertyInstance();
-      hyperty.setDescriptor("addd12AAQQ");
-      services.createUserHyperty(user, newHypertyID, hyperty);
-      Map<String, HypertyInstance> allHyperties = services.getAllHyperties(user);
-      assertEquals(2, allHyperties.keySet().size());
+      HypertyInstance hypertyInstance = new HypertyInstance();
+      String hypertyID = "hyperty://ua.pt/123-ba-123123";
+      String descriptor = "hyperty-catalogue://ua.pt/1/123-ba-123123";
+      hypertyInstance.setDescriptor(descriptor);
+      services.createUserHyperty(userID, hypertyID, hypertyInstance);
+      Map<String, HypertyInstance> allUserHyperties = services.getAllHyperties(userID);
+      assertEquals(2, allUserHyperties.keySet().size());
     }
 
     @Test(expected = UserNotFoundException.class) 
     public void getAllHypertiesNonexistingUserTest(){
-        services.getAllHyperties("jj@twitter.com");
+        services.getAllHyperties("user://inesc-id.pt/ruipereira");
     }
 
     @Test
     public void updateHypertyInfoTest(){
-        Map<String, HypertyInstance> servs = services.getServices().get(user);
-        HypertyInstance hyperty = new HypertyInstance();
-        hyperty.setDescriptor("asdsa111d");
-        services.createUserHyperty(user, hypertyID, hyperty);
-        assertTrue(servs.containsKey(hypertyID));
-        HypertyInstance newHyperty = servs.get(hypertyID);
-        String d1 = newHyperty.getDescriptor();
-        String d2 = hyperty.getDescriptor();
-        assertEquals(d1, d2);
+        HypertyInstance hypertyInstance = new HypertyInstance();
+        String descriptor = "hyperty-catalogue://inesc-id.pt/1/222-ba-123123";
+        hypertyInstance.setDescriptor(descriptor);
+        Map<String, HypertyInstance> allUserHyperties = services.getServices().get(userID);
+        services.createUserHyperty(userID, hypertyID, hypertyInstance);
+        assertTrue(allUserHyperties.containsKey(hypertyID));
+        HypertyInstance newHypertyInstance = allUserHyperties.get(hypertyID);
+        String oldHypertyDescriptor = newHypertyInstance.getDescriptor();
+        String updatedHypertyDescriptor = hypertyInstance.getDescriptor();
+        assertEquals(oldHypertyDescriptor, updatedHypertyDescriptor);
     }
 
     @Test
     public void removeUserHypertyTest(){
-        services.deleteUserHyperty(user, hypertyID);
-        assertFalse(services.getServices().get(user).containsKey(hypertyID));
+        services.deleteUserHyperty(userID, hypertyID);
+        assertFalse(services.getServices().get(userID).containsKey(hypertyID));
     }
 
     @Test(expected= UserNotFoundException.class) 
     public void removeFromANonExistantUserTest(){
-        services.deleteUserHyperty("jj@twitter.com", hypertyID);
+        services.deleteUserHyperty("user://inesc-id.pt//pedro", hypertyID);
     }
 
     @Test(expected= DataNotFoundException.class)
     public void removeANonexistentHypertyTest(){
-        String nonexistentHypertyID = "sdasdsa111112";
-        services.deleteUserHyperty(user, nonexistentHypertyID);
+        String nonExistentHypertyID = "hyperty://inesc-id.pt//asda-123-ll";
+        services.deleteUserHyperty(userID, nonExistentHypertyID);
     }
 }
 
