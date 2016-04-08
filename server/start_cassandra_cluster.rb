f943c1f2c32e0714d31aefd462e76e636d7171e6
ruijose <ruijosemangas@gmail.com>
2016-04-08 13:45:57 +0100

    added license header to cassandra script

--- server/start_cassandra_cluster.rb
+++ server/start_cassandra_cluster.rb
@@ -1,3 +1,21 @@
+# Copyright 2015-2016 inesc-id-ID
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# 1. For cleanup purposes all previous cassandra containers are removed from the remote servers
+# 2. Seeds nodes are initialized
+# 3. Remaining nodes are initialized with a 2 minute delay between them
+
 puts "This tool starts a Cassandra DB cluster with predefined servers"
 
 nodes_ips = []
@@ -8,9 +26,9 @@ ARGV[1..-1].each { |node| p node ; nodes_ips << node }
 number_of_nodes = nodes_ips.size
 number_of_nodes > 3 ? number_of_seeds = number_of_nodes / 2 : number_of_seeds = 1
 
-#stop and removing existing containers
+#stop and removing existing cassandra containers
 nodes_ips.each do |node|
-  cmd = "ssh #{remote_username}@#{node} 'docker rm -f $(docker ps -a -q)'"
+  cmd = "ssh #{remote_username}@#{node} "docker ps -a | grep 'cassandra' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f""
   system(cmd)
 end
 
