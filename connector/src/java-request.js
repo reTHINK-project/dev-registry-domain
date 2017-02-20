faa10f6110b5358a9f3baa1165fbc2bccbc67c4a
Nuno Nogueira <nunofmn@gmail.com>
2017-02-20 15:01:43 +0000

    Change Java connector request layer.

--- connector/src/java-request.js
+++ connector/src/java-request.js
@@ -15,75 +15,46 @@
 *
 */
 
-var trustStoreBuffer = vertx.fileSystem().readFileBlocking("ca-bundle.pem");
-var clientKeyBuffer = vertx.fileSystem().readFileBlocking("connector.key.pem");
-var clientCertBuffer = vertx.fileSystem().readFileBlocking("connector.cert.pem");
-
-
-var options  = {
-  "ssl": true,
-  "pemTrustOptions": {
-    "certValues": [
-      trustStoreBuffer
-    ]
-  },
-  "pemKeyCertOptions": {
-    "keyValue": clientKeyBuffer,
-    "certValue": clientCertBuffer
-  }
-};
-
-var client = vertx.createHttpClient(options);
-
+var HttpRequest = Java.type('eu.rethink.mn.util.HttpRequest');
+var client = new HttpRequest();
 
 var JavaRequest = {
 
   get: function(url, callback) {
-    client.getAbs(url, function (response) {
-      response.bodyHandler(function(totalBuffer) {
-        var body = totalBuffer.toString("UTF-8");
-        callback(null, JSON.parse(body), response.statusCode());
-      });
-    })
-    .exceptionHandler(function(e) {
-      print("[REGISTRY-CONNECTOR] Error: " + e);
-      callback(e, null, null);
-    })
-    .end();
+     try {
+       var response = client.get(url);
+       var parsedResponse = JSON.parse(response);
+
+       print(response);
+       callback(null, JSON.parse(parsedResponse.data), response.code);
+     } catch(e) {
+       e.printStackTrace();
+     }
   },
 
   put: function(url, data, callback) {
-    var finalData = JSON.stringify(data);
+     try {
+       var finalData = JSON.stringify(data);
+       var response = client.put(url, finalData);
+       var parsedResponse = JSON.parse(response);
 
-    client.putAbs(url)
-    .putHeader("content-type", "application/json")
-    .putHeader("content-length", "" + finalData.length())
-    .handler(function(response) {
-      response.bodyHandler(function(totalBuffer) {
-        var body = totalBuffer.toString("UTF-8");
-        callback(null, JSON.parse(body), response.statusCode());
-      });
-    })
-    .exceptionHandler(function(e) {
-      print("[REGISTRY-CONNECTOR] Error: " + e);
-      callback(e, null, null);
-    })
-    .write(finalData)
-    .end();
+       print(response);
+       callback(null, JSON.parse(parsedResponse.data), response.code);
+     } catch(e) {
+       e.printStackTrace();
+     }
   },
 
   del: function(url, callback) {
-    client.deleteAbs(url, function(response) {
-      response.bodyHandler(function(totalBuffer) {
-        var body = totalBuffer.toString("UTF-8");
-        callback(null, JSON.parse(body), response.statusCode());
-      });
-    })
-    .exceptionHandler(function(e) {
-      print("[REGISTRY-CONNECTOR] Error: " + e);
-      callback(e, null, null);
-    })
-    .end();
+     try {
+       var response = client.del(url);
+       var parsedResponse = JSON.parse(response);
+
+       print(response);
+       callback(null, JSON.parse(parsedResponse.data), response.code);
+     } catch(e) {
+       e.printStackTrace();
+     }
   }
 
 };
